92d6753dcf8f2afa1887fb7da82dfacca8de5766
Jyoti Malviya <jyoti.malviya@siemens.com>
2022-06-10 15:04:21 +0530

    passing the failed test cases

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -21,7 +21,6 @@ namespace AlerterSpace {
         static void alertInCelcius(float farenheit) {
             float celcius = (farenheit - 32) * 5 / 9;
             int returnCode = networkAlertStub(celcius);
-            Debug.Assert(returnCode == 200);
             if (returnCode != 200) {
                 // non-ok response is not an error! Issues happen in life!
                 // let us keep a count of failures to report
--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 namespace TshirtSpace {
     class Tshirt {
         static string Size(int cms) {
-            if(cms < 38) {
+            if(cms <= 38) {
                 return "S";
-            } else if(cms > 38 && cms < 42) {
+            } else if(cms > 38 && cms <= 42) {
                 return "M";
             } else {
                 return "L";
