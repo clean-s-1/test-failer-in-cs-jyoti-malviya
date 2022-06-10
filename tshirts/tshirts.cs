f4363a91d582d3708ad0324770db7efa144ec5c0
Jyoti Malviya <jyoti.malviya@siemens.com>
2022-06-10 14:49:27 +0530

    done changes in tshirts project

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -4,16 +4,16 @@ using System.Diagnostics;
 namespace TshirtSpace {
     class Tshirt {
         static string Size(int cms) {
-            if(cms <= 38) {
+            if(cms < 38) {
                 return "S";
-            } else if(cms > 38 && cms <= 42) {
+            } else if(cms > 38 && cms < 42) {
                 return "M";
             } else {
                 return "L";
             }
         }
         static void Main(string[] args) {
-            Debug.Assert(Size(37) == "S");
+            Debug.Assert(Size(38) == "S");
             Debug.Assert(Size(40) == "M");
             Debug.Assert(Size(43) == "L");
             Console.WriteLine( "All is well (maybe!)");
